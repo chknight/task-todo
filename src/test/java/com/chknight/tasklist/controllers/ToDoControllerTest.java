4754b288b2e4b1c4f9525173c4785c8702b82fcb
Tom <ch.knight05@gmail.com>
2021-02-07 20:33:41 +1000

    Add test for api endpoint

--- src/test/java/com/chknight/tasklist/controllers/ToDoControllerTest.java
+++ src/test/java/com/chknight/tasklist/controllers/ToDoControllerTest.java
@@ -1,7 +1,11 @@
 package com.chknight.tasklist.controllers;
 
 import com.chknight.tasklist.dtos.ToDoItemDTO;
+import com.chknight.tasklist.exceptions.ToDoItemNotFoundException;
+import com.chknight.tasklist.exceptions.ToDoItemValidationException;
 import com.chknight.tasklist.services.ToDoService;
+import com.chknight.tasklist.shared.ErrorDetailMessage;
+import com.chknight.tasklist.shared.GenericErrorMessage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -10,10 +14,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -48,6 +54,92 @@ public class ToDoControllerTest {
         );
     }
 
+    @Test
+    public void shouldReturn400IfInputIsTooShortWhenCreatingToDoItem() throws Exception {
+        String testString = "";
+
+        mockInvalidException(testString);
+
+        mockMvc.perform(
+                post("/todo")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(String.format("{\"text\": \"%s\"}", testString))
+        ).andExpect(
+                status().isBadRequest()
+        ).andExpect(
+                jsonPath("$.name", is("ValidationError"))
+        ).andExpect(
+                jsonPath("$.details[0].location", is("params"))
+        ).andExpect(
+                jsonPath("$.details[0].param", is("text"))
+        ).andExpect(
+                jsonPath("$.details[0].msg", is("Must be between 1 and 50 chars long"))
+        ).andExpect(
+                jsonPath("$.details[0].value", is(""))
+        );
+    }
+
+    @Test
+    public void shouldReturn400IfInputIsTooLongWhenCreatingToDoItem() throws Exception {
+        String testString = "a".repeat(51);
+
+        mockInvalidException(testString);
+
+        mockMvc.perform(
+                post("/todo")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(String.format("{\"text\": \"%s\"}", testString))
+        ).andExpect(
+                status().isBadRequest()
+        ).andExpect(
+                jsonPath("$.name", is("ValidationError"))
+        ).andExpect(
+                jsonPath("$.details[0].location", is("params"))
+        ).andExpect(
+                jsonPath("$.details[0].param", is("text"))
+        ).andExpect(
+                jsonPath("$.details[0].msg", is("Must be between 1 and 50 chars long"))
+        ).andExpect(
+                jsonPath("$.details[0].value", is(""))
+        );
+    }
+
+    @Test
+    public void shouldReturnToDoItemDTOWhenFindingDataSuccessfully() throws Exception {
+        Long id = 1L;
+        ToDoItemDTO result = createToDoDTO("test");
+
+        when(toDoService.findToDoItem(id)).thenReturn(result);
+
+        mockMvc.perform(
+                get("/todo/{id}", id)
+        ).andExpect(
+                status().isOk()
+        ).andExpect(
+                jsonPath("$.id", is(result.getId().intValue()))
+        ).andExpect(
+                jsonPath("$.text", is(result.getText()))
+        ).andExpect(
+                jsonPath("$.isCompleted", is(result.getIsCompleted()))
+        );
+    }
+
+    @Test
+    public void shouldReturn404IfCouldNotFoundById() throws Exception {
+        Long id = 1L;
+        mockNotFoundException(id);
+
+        mockMvc.perform(
+                get("/todo/{id}", id)
+        ).andExpect(
+                status().isNotFound()
+        ).andExpect(
+                jsonPath("$.name", is("NotFoundError"))
+        ).andExpect(
+                jsonPath("$.details[0].message", is("Item with 1 not found"))
+        );
+    }
+
     private ToDoItemDTO createToDoDTO(String text) {
         return new ToDoItemDTO(
                 1L,
@@ -56,4 +148,30 @@ public class ToDoControllerTest {
                 LocalDateTime.now()
         );
     }
-}
+
+    private void mockInvalidException(String text) throws ToDoItemValidationException {
+        when(toDoService.createToDoItem(text)).thenThrow(
+                new ToDoItemValidationException(
+                        List.of(
+                                ErrorDetailMessage.build(
+                                        "params",
+                                        "text",
+                                        "Must be between 1 and 50 chars long",
+                                        ""
+                                )
+                        )
+                )
+        );
+    }
+
+    private void mockNotFoundException(Long id) throws ToDoItemNotFoundException {
+        when(toDoService.findToDoItem(id)).thenThrow(
+                new ToDoItemNotFoundException(
+                        List.of(
+                                GenericErrorMessage.build(
+                                        String.format("Item with %d not found", id)
+                                )
+                        )
+                )
+        );
+    }}
